dd8fcc340485f13ef663d9908f0048f260147a1e
john <john@johns-MacBook.local>
2009-10-30 13:11:34 -0400

    signed objects with input and output and uuids oh my

--- src/dunjeon/security.clj
+++ src/dunjeon/security.clj
@@ -1,16 +1,62 @@
-(ns dunjeon.security)
+(ns dunjeon.security
+  (:import java.security.MessageDigest java.util.UUID))
 
-(defn create-token
-  "creates security token"
+(defn uuid
+  "Returns a random UUID"
   []
-  "foobar")
+  (.toString (UUID/randomUUID)))
 
-(defn secure-object
-  "generates and attaches a security token to ab object"
+(defn- bytes-to-hex-str
+  "Does what it says on the tin"
+  [byte-array]
+  (apply str (map #(format "%02x" (bit-and % 0xff)) byte-array)))
+
+(defn- sha-hash
+  "Returns the SHA hash of string s"
+  [s]
+  (bytes-to-hex-str
+   (.digest
+    (doto (MessageDigest/getInstance "SHA-1") (.update (.getBytes s))))))
+
+(defn- salted-hash
+  [s]
+  (sha-hash (str "your salt here" s)))
+
+(defn- object-token
+  "Creates security token for a given object"
+  [x hash-fn]
+  (hash-fn (pr-str x)))
+
+(defn new-object
+  "Returns a new object with a unique id"
   []
-  #^{:dunjeon-token (create-token)} {})
+  {:id (uuid)})
 
-(defn validate-object
-  "validates that a given object was created by this instance"
+(def *token-key* :obj-hash)
+
+(defn sign
+  "Returns an object with a verifiable signature"
   [x]
-  (= (:dunjeon-token ^x) (create-token)))
+  (with-meta x (assoc ^x *token-key* (object-token x salted-hash))))
+
+(defn auth
+  "Verifies the authenticity of a signed object"
+  [x]
+  (= (^x *token-key*) (object-token x salted-hash)))
+
+(defn pr-signed-str
+  "Returns the signed object in a readable format"
+  [x]
+  (let [signed (sign x)]
+    (pr-str [(^signed *token-key*) signed])))
+
+(defn pr-signed
+  "Writes the an object with a signature in a readable format"
+  [x]
+  (print (pr-signed-str x)))
+
+(defn read-signed-string
+  "Returns an objecy by reading a string representation"
+  [s]
+  (let [v (read-string s)]
+    (with-meta (v 1) {*token-key* (v 0)})))
