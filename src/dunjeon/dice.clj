69f259f199f57d8bf3a91fe9934ba2097bf2960e
John Cromartie <jcromartie@gmail.com>
2009-11-21 17:49:03 -0500

    better dice?

--- src/dunjeon/dice.clj
+++ src/dunjeon/dice.clj
@@ -2,19 +2,15 @@
 
 (defn roll
   "Rolls an n-sided die"
-  [n]
-  (inc (rand-int n)))
+  ([n] (inc (rand-int n)))
+  ([times n] (apply + (take times (repeatedly #(roll n))))))
 
 (defn aced-roll
   "Rolls an n-sided die with Savage Worlds acing. Returns a list of rolls"
   [n]
   (loop [this-roll (roll n)
 	 rolls []]
-    (if-not (= n this-roll)
-      (conj rolls this-roll)
-      (recur (roll n) (conj rolls this-roll)))))
-
-(defn sum
-  "Adds all items in coll"
-  [coll]
-  (apply + coll))
+    (let [result (conj rolls this-roll)]
+      (if (= n this-roll)
+	(recur (roll n) result)
+	result))))
